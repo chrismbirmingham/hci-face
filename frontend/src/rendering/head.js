bab658466e76f62aadf188e87fc452eca74745a6
Chris <cmbirmingham19@gmail.com>
2023-01-10 14:16:09 -0800

    Put QT frame around face

--- frontend/src/rendering/head.js
+++ frontend/src/rendering/head.js
@@ -14,9 +14,14 @@ function Head({position, eyeAU, browAU, mouthAU}){
                   <stop offset="85%" stopColor="#D7E4F5" />
                   <stop offset="100%" stopColor="#a0c9ff" />
                 </radialGradient>
+                {/* fill="url(#sampleGradient)" */}
               </defs>
-              {/* <rect x="-50" y="-50" rx="25" ry="35" width="99" height="99" fill="url(#sampleGradient)" stroke="#FFF" strokeWidth=".5"/> */}
-              <rect x="-50" y="-50" width="99" height="99" fill="#D7E4F5" stroke="#FFF" strokeWidth=".5"/>
+              <rect id="neck" x="-10" y="39" rx="15" ry="15" width="20" height="20" fill="blue" stroke="grey" strokeWidth=".5"/>
+              <rect x="-50" y="-50" rx="35" ry="15" width="100" height="95" fill="#FFF" stroke="#FFF" strokeWidth=".5"/>
+              <rect id="face" x="-45" y="-30" width="90" height="60" fill="#D7E4F5" stroke="#EEE" strokeWidth="2"/>
+              <rect id="camera" x="-25" y="-43" rx="5" ry="5" width="50" height="7" fill="#000" stroke="#FFF" strokeWidth=".5"/>
+              <rect id="camera2" x="-15" y="-42" rx="5" ry="5" width="5" height="5" fill="grey" stroke="#111" strokeWidth=".5"/>
+              <rect id="camera2" x="-5" y="-42" rx="5" ry="5" width="5" height="5" fill="grey" stroke="#111" strokeWidth=".5"/>
               <Eye id="right" position={position.right_eye} eyeAU={eyeAU}/>
               <Eye id="left" position={position.left_eye} eyeAU={eyeAU}/> 
               <Brow id="leftbrow" position={position.left_brow} browAU={browAU}/>
