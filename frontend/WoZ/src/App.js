f317779359c90582444ad9c9c5587745a0fd34a2
Chris <cmbirmingham19@gmail.com>
2023-01-24 13:13:05 -0800

    Integrate timer with WoZ

--- frontend/WoZ/src/App.js
+++ frontend/WoZ/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { ReactMic } from './react-mic-clone';
-import CountdownTimer from './timer';
 
 const stt_socket = new W3CWebSocket('ws://localhost:8000/api/stt');
 
@@ -20,6 +19,7 @@ const App = ({ classes }) => {
 
   const [speakerVoice, setSpeakerVoice] = useState("p270");
   const [participantSpeaker, setParticipantSpeaker] = useState("");
+  const [priorSpeaker, setPriorSpeaker] = useState("");
   const [latestSpeech, setLatestSpeech] = useState("");
   const [classifications, setClassifications] = useState("");
   const [transcribedData, setTranscribedData] = useState([""]);
@@ -28,7 +28,63 @@ const App = ({ classes }) => {
   const [expression, setExpression] = useState("");
   const [behavior, setBehavior] = useState("focused");
   
+	// The state for our timer
+  const Ref = useRef(null);
+	const [timer, setTimer] = useState('00:00:00');
+	const [minuteGoal, setMinuteGoal] = useState('15');
+	const [color, setColor] = useState('Green');
+	const [date, setDate] = useState("")
+	// const [playTimerVar, setPlay] = useState(true)
 
+
+	const getTimeRemaining = (e) => {
+		const total = Date.parse(e) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+		setDate(Date())
+		return {
+			total, hours, minutes, seconds
+		};
+	}
+
+	const updateTimerDisplay = (e) => {
+		let { total, hours, minutes, seconds } = getTimeRemaining(e);
+		if (total >= 0) {
+			setTimer(
+				(hours > 9 ? hours : '0' + hours) + ':' +
+				(minutes > 9 ? minutes : '0' + minutes) + ':'
+				+ (seconds > 9 ? seconds : '0' + seconds)
+			)
+		}
+		if (minutes >= 3){setColor("green")}
+		if (minutes < 3 && minutes >= 1){
+      setWalkthroughToggle(false)
+      setColor("orange")
+    }
+		if (minutes < 1 ){setColor("red")}
+	}
+
+	const runTimer = (e) => {
+		console.log("run timer")
+		if (Ref.current) clearInterval(Ref.current);
+		const id = setInterval(() => {
+			
+			// console.log("Update Timer Display", playTimerVar)
+			updateTimerDisplay(e);
+
+		}, 1000)
+		Ref.current = id;
+	}
+
+	const getDeadTime = (m) => {
+		console.log("New goal: ", m)
+		let deadline = new Date();
+		let newseconds = m*60 + 1
+		deadline.setSeconds(deadline.getSeconds() + newseconds);
+		return deadline;
+	}
+	
   /////// Socket Handler ///////
   stt_socket.onopen = () => {console.log({ event: 'onopen' })};
   stt_socket.onclose = () => {console.log({ event: 'onclose' })}
@@ -42,18 +98,35 @@ const App = ({ classes }) => {
      }
   }
 
+
   /////// Process STT input and get bot response ///////
   async function process_stt(received) {
+    var speechToBot
     setTranscribedData(oldData => [participantSpeaker+":"+received,  <br></br>, ...oldData ]);
-    setLatestSpeech(received);
+    if (participantSpeaker === priorSpeaker){
+      speechToBot = latestSpeech + " " + received;
+      setLatestSpeech(latestSpeech + " " + received)
+    }
+    else {
+      setLatestSpeech(received);
+      speechToBot = received
+    }
+
+    setPriorSpeaker(participantSpeaker);
     setClassifications("");
-    const raw_res = await get_bot_response(received);
-    const respArray = raw_res.split("&&&");
-    setTreeResponse(respArray[0]);
-    setBotResponse(respArray[1]);
-    setClassifications(respArray[2]);
+
+    if (speechToBot.length > 20) {
+      console.log(speechToBot, speechToBot.length)
+      const raw_res = await get_bot_response(speechToBot);
+      console.log("raw response ", raw_res)
+      const respArray = raw_res.split("&&&");
+      setTreeResponse(respArray[0]);
+      setBotResponse(respArray[1]);
+      setClassifications(respArray[2]);
+    }
     setBeginConversation(false);
   }
+
   async function get_bot_response(human_input) {
     const text = human_input
     const speaker = participantSpeaker
@@ -67,6 +140,7 @@ const App = ({ classes }) => {
   /////// Get and Play Speech ///////
   function do_tts(e) {
     console.log("Will say"+e)
+    setPriorSpeaker("bot")
     const text = e
     const speaker_id = [speakerVoice]
     const style_wav = ""
@@ -91,6 +165,10 @@ const App = ({ classes }) => {
 
   function get_preset(name) {
     const text = name
+    if (text==="f_invitation"){
+      runTimer(getDeadTime(15))
+      setWalkthroughToggle(true)
+    }
     if (text) {
       return fetch(`//localhost:8000/api/facilitator_buttons?text=${encodeURIComponent(text)}`, { cache: 'no-cache' })
       .then(response => response.text())
@@ -140,25 +218,29 @@ const App = ({ classes }) => {
             backgroundColor="#000" 
             height={60}
             width={320} />
+          <button id="Recording" onClick={() => setIsRecording(true)}>Start Listening </button>
         </div>      
       </div>
-      <CountdownTimer/>
-      <br></br>
-      <button id="toggle" onClick={() => setWalkthroughToggle(!showWalkthrough)}>Show/Hide Walkthrough</button>
-      <div id="walkthrough" hidden={showWalkthrough}>
-      <h2>Study Walkthrough:</h2>
       
-        1- Start by reviewing consent <br></br>
+      <br></br>
+      <button id="toggle" onClick={() => setWalkthroughToggle(!showWalkthrough)}>Show/Hide Study Walkthrough: </button>
+      <div id="walkthrough" hidden={showWalkthrough}>
+      {/* <h2>Study Walkthrough:</h2> */}
+      
+        1- Start by reviewing consent <br></br> 
         ---  Next provide link to survey:    https://usc.qualtrics.com/jfe/form/SV_diE2Ow6GQPlSCP4 <br></br>
         ---  WoZ prompt participants to complete first question set. (4 pages)
         <br></br>
+          <button onClick={() => do_tts("Testing, 1, 2, 3. Can you all hear me?")}>Speech Test</button>
         <br></br>
         2- <button onClick={() => get_preset("f_qt-intro")}>QT introduction</button>--
+          <button onClick={() => get_preset("g_QT/hi")}>wave</button>--
         <button style={{backgroundColor:"grey"}} onClick={() => get_preset("f_survey-prompt")}>survey prompt</button>--
         <button style={{backgroundColor:"grey"}} onClick={() => get_preset("f_survey-return")}>survey return</button>
         <br></br>
         <br></br>
-        3- <button onClick={() => get_preset("f_group-intro")}>group introductions</button>--(let them respond)--
+        3- <button onClick={() => get_preset("f_group-intro")}>group introductions</button>--
+          <button onClick={() => get_preset("g_QT/emotions/shy")}>shy</button>--(let them respond)--
         <button style={{backgroundColor:"green"}} onClick={() => get_preset("f_invitation")}>invitation to start</button>--
         <button style={{backgroundColor:"orange"}} onClick={() => get_preset("f_closing")}>closing</button>--
         <button style={{backgroundColor:"red"}} onClick={() => get_preset("f_transition")}>End section</button>--
@@ -186,16 +268,18 @@ const App = ({ classes }) => {
       </div>
 
       <div id="controls">
-        <h3 style={{backgroundColor:"grey"}}>Interactive Controls:</h3>
+        <h3 style={{backgroundColor:color}}>Interactive Controls:</h3>
+			  <p style={{backgroundColor:color}}>Time Remaining: {timer}</p>
         
         <div onChange={(e) => setParticipantSpeaker(e.target.value)}>The current speaker:
-          <label> <input type="radio" value="Libby" name="speaker" text="Libby" /> Libby</label>
-          <label> <input type="radio" value="Chris" name="speaker" /> Chris</label>
-          <label> <input type="radio" value="Lynn" name="speaker" /> Lynn</label>
-          <label> <input type="radio" value="Human" name="speaker" /> Default</label>
+          <label> <input type="radio" value="Nathan" name="speaker" /> Nathan</label>
+          <label> <input type="radio" value="Lauren" name="speaker" /> Lauren</label>
+          <label> <input type="radio" value="Mina" name="speaker" /> Mina</label>
+          <label> <input type="radio" value="Participant" name="speaker" /> Default</label>
           <label> <input type="radio" value="" name="speaker" /> None</label>
         </div>
-        Said: {latestSpeech}
+        <br></br>
+        {participantSpeaker} Said: {latestSpeech}
         <br></br>
         Classified as: {classifications}
         
@@ -208,7 +292,7 @@ const App = ({ classes }) => {
         Recommended Statement:<button style={{backgroundColor:"Chartreuse"}} onClick={() => do_tts(participantSpeaker+". "+treeResponse)}>{participantSpeaker+". "+treeResponse}</button>
         <br></br>
           <button onClick={() => get_preset("r_disclosure")}>Make Disclosure</button>--
-          <button onClick={() => do_tts(participantSpeaker+". "+treeResponse)}>Say Response: {treeResponse}</button></div>
+          <button onClick={() => get_preset("r_response")}>Say Response</button></div>
         <br></br>
 
         {/* <h4>Utility Responses</h4> */}
@@ -218,9 +302,13 @@ const App = ({ classes }) => {
           <button style={{backgroundColor:"BlueViolet"}} onClick={() => do_tts("Thank you, "+participantSpeaker)}>Thank You</button>---
           <button style={{backgroundColor:"orange"}} onClick={() => do_tts(participantSpeaker+" can you repeat that? I didn't hear you.")}>Please repeat</button>---
           <button onClick={() => do_tts("I am unsure how to answer that. sorry.")}>Unsure</button>----
-          <button onClick={() => do_tts("Is there anything anyone would like to talk about?")}>Prompt</button>----
+          <button onClick={() => do_tts("Is there anything anyone would like to talk about? Are there any challenges or struggles you are working through?")}>Prompt</button>----
+        <br></br><br></br>
+          Gestures:
           <button onClick={() => get_preset("g_QT/emotions/happy")}>happy</button>--
           <button onClick={() => get_preset("g_QT/emotions/shy")}>shy</button>--
+          <button onClick={() => get_preset("g_QT/emotions/sad")}>sad</button>--
+          <button onClick={() => get_preset("g_QT/hi")}>wave</button>--
         <br></br><br></br>
         Say ChatBot Response: <button style={{backgroundColor:"pink"}} onClick={() => do_tts(botResponse)}>{botResponse}</button>
         <br></br>
@@ -234,10 +322,10 @@ const App = ({ classes }) => {
           <select value={behavior} 
               multiple={false}
               onChange={(e) => update_behavior(e.target.value)}>
+              <option value="none">none</option>
               <option value="focused">focused</option>
               <option value="random">random</option>
               <option value="bored">bored</option>
-              <option value="none">none</option>
             </select>
           </label>
           <label>Expression:
@@ -290,7 +378,9 @@ const App = ({ classes }) => {
             />
           </label>
           </div>
-          <label>Enter the text you would like the robot to say:
+          Set timer for: <input value={minuteGoal} type="text" size={3} onChange={(e) => setMinuteGoal(e.target.value)}/>
+			  <button onClick={() => runTimer(getDeadTime(minuteGoal))}>Run Timer</button> --- Current Date: {date}
+        <br></br><label>Enter the text you would like the robot to say:
             <textarea 
               cols={100}
               rows={4}
