55131f0c1e48f3593d58cc3b2c4d0255f0eeb5b8
Chris <cmbirmingham19@gmail.com>
2023-01-31 08:34:33 -0800

    Update logging and linting the mic

--- frontend/WoZ/src/components/mic/Microphone.js
+++ frontend/WoZ/src/components/mic/Microphone.js
@@ -4,15 +4,17 @@ import useRecorder from "./useRecorder";
 
 export default function Mic ({isRecording}) {
   const [listening, setListen] = useState(false);
-  const [audio, setAudio] = useState(null) // eslint-disable-next-line
+  const [audio, setAudio] = useState(null)
   const [rmsThresh, setRMSThresh] = useState(.005)
   const [bufferSize, setBufferSize] = useState(20)
   const [hearing, setHearing] = useState(false)
-  const { recorderState, ...handlers } = useRecorder(); // eslint-disable-next-line
+  const { recorderState, ...handlers } = useRecorder(); 
+  // eslint-disable-next-line
   const { startRecording, cancelRecording, saveRecording } = handlers;
 
 
   async function getMicrophone() {
+    console.log("Starting up mic")
     setListen(true)
     const audioDevice = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -22,12 +24,14 @@ export default function Mic ({isRecording}) {
   }
 
   function stopMicrophone() {
+    console.log("Closing down mic")
     setListen(false)
     audio.getTracks().forEach(track => track.stop());
     setAudio(null );
   }
 
   function toggleMicrophone() {
+    console.log("Toggling mic")
     if (audio) {
       stopMicrophone();
     } else {
@@ -44,20 +48,22 @@ export default function Mic ({isRecording}) {
     if (!listening && isRecording) {
       console.log("toggle on")
       getMicrophone()
-    }
+    }// eslint-disable-next-line
   },[isRecording])
 
   useEffect(() => {
+    console.log("There was a change in hearing:", hearing)
     if (hearing) {
       startRecording()
     }
     if (!hearing) {
       saveRecording()
-    }
+    }// eslint-disable-next-line
   },[hearing])
 
 
   const wrappingSetHearing = useCallback(val => {
+    console.log("Set hearing callback hit with value:", val)
     setHearing(val);
   }, [setHearing])
 
